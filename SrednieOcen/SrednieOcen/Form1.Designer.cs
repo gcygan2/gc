b71df806d6e1726fca8bb2814f69507fee23a9b9
gcygan2 <114261165+gcygan2@users.noreply.github.com>
2024-03-12 20:03:23 +0100

    12032024K

---
+++ SrednieOcen/SrednieOcen/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace SrednieOcen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPolski = new System.Windows.Forms.TextBox();
+            this.txtObcy = new System.Windows.Forms.TextBox();
+            this.txtMIESL = new System.Windows.Forms.TextBox();
+            this.txtUTK = new System.Windows.Forms.TextBox();
+            this.txtProg = new System.Windows.Forms.TextBox();
+            this.txtWynik = new System.Windows.Forms.TextBox();
+            this.lab = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "język polski";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "język obcy";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "urządzenia techniki komputerowej";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(187, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "montaż i eksploatacja sieci lokalnyhch";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(34, 155);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "prorgamowanie";
+            // 
+            // txtPolski
+            // 
+            this.txtPolski.Location = new System.Drawing.Point(242, 31);
+            this.txtPolski.Name = "txtPolski";
+            this.txtPolski.Size = new System.Drawing.Size(100, 20);
+            this.txtPolski.TabIndex = 5;
+            // 
+            // txtObcy
+            // 
+            this.txtObcy.Location = new System.Drawing.Point(242, 62);
+            this.txtObcy.Name = "txtObcy";
+            this.txtObcy.Size = new System.Drawing.Size(100, 20);
+            this.txtObcy.TabIndex = 6;
+            // 
+            // txtMIESL
+            // 
+            this.txtMIESL.Location = new System.Drawing.Point(242, 93);
+            this.txtMIESL.Name = "txtMIESL";
+            this.txtMIESL.Size = new System.Drawing.Size(100, 20);
+            this.txtMIESL.TabIndex = 7;
+            // 
+            // txtUTK
+            // 
+            this.txtUTK.Location = new System.Drawing.Point(242, 124);
+            this.txtUTK.Name = "txtUTK";
+            this.txtUTK.Size = new System.Drawing.Size(100, 20);
+            this.txtUTK.TabIndex = 8;
+            // 
+            // txtProg
+            // 
+            this.txtProg.Location = new System.Drawing.Point(242, 152);
+            this.txtProg.Name = "txtProg";
+            this.txtProg.Size = new System.Drawing.Size(100, 20);
+            this.txtProg.TabIndex = 9;
+            // 
+            // txtWynik
+            // 
+            this.txtWynik.Location = new System.Drawing.Point(242, 225);
+            this.txtWynik.Name = "txtWynik";
+            this.txtWynik.Size = new System.Drawing.Size(100, 20);
+            this.txtWynik.TabIndex = 11;
+            // 
+            // lab
+            // 
+            this.lab.AutoSize = true;
+            this.lab.Location = new System.Drawing.Point(34, 228);
+            this.lab.Name = "lab";
+            this.lab.Size = new System.Drawing.Size(37, 13);
+            this.lab.TabIndex = 10;
+            this.lab.Text = "Wynik";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(242, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Oblicz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 291);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtWynik);
+            this.Controls.Add(this.lab);
+            this.Controls.Add(this.txtProg);
+            this.Controls.Add(this.txtUTK);
+            this.Controls.Add(this.txtMIESL);
+            this.Controls.Add(this.txtObcy);
+            this.Controls.Add(this.txtPolski);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Srednia ocen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPolski;
+        private System.Windows.Forms.TextBox txtObcy;
+        private System.Windows.Forms.TextBox txtMIESL;
+        private System.Windows.Forms.TextBox txtUTK;
+        private System.Windows.Forms.TextBox txtProg;
+        private System.Windows.Forms.TextBox txtWynik;
+        private System.Windows.Forms.Label lab;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SrednieOcen/SrednieOcen/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SrednieOcen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            float polski = float.Parse(txtPolski.Text);
+            float obcy = float.Parse(txtObcy.Text);
+            float miesl = float.Parse(txtMIESL.Text);
+            float utk = float.Parse(txtUTK.Text);
+            float programowanie = float.Parse(txtProg.Text);
+
+            float srednia = (polski + obcy + miesl + utk + programowanie) / 5;
+            txtWynik.Text = srednia.ToString();
+        }
+    }
+}
