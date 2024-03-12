b8e927b056682b7d4e02acfbcb9d9d2bfcdc7085
gcygan2 <114261165+gcygan2@users.noreply.github.com>
2024-03-12 19:38:28 +0100

    12032024

---
+++ Rozgrzewka12032024/Rozgrzewka12032024/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozgrzewka12032024
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę, którą chcesz pomnożyć");
+            string podanaLiczba = Console.ReadLine();
+            int lPomnozona = int.Parse(podanaLiczba) * 2;
+            Console.WriteLine ($"Powiększyłem twoją liczbę dwukrotnie i wyszło {lPomnozona}");
+            Console.ReadLine();
+        }
+    }
+}
