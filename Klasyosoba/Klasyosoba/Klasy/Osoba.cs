4b884f8d1b649c55ddbe78dc647eb28d1613b370
gcygan2 <114261165+gcygan2@users.noreply.github.com>
2024-03-19 20:09:57 +0100

    19032024K

---
+++ Klasyosoba/Klasyosoba/Klasy/Osoba.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasyosoba.Klasy
+{
+    class Osoba
+    {
+        private string Imie;
+        private string Nazwisko;
+        private static int Licznik = 0;
+        
+        public Osoba(string imie, string nazwisko)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Licznik++;
+        }
+        public void WyswietlLicznik()
+        {
+            Console.WriteLine(Licznik);
+        }
+
+    }
+}
---
+++ Klasyosoba/Klasyosoba/Program.cs
@@ -0,0 +1,21 @@
+﻿using Klasyosoba.Klasy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasyosoba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba osoba = new Osoba("Dawid", "Małkiewiz");
+            osoba.WyswietlLicznik();
+            Osoba osoba2 = new Osoba("Grzegorz", "Cygan");
+            osoba.WyswietlLicznik();
+            Console.ReadKey();
+        }
+    }
+}
