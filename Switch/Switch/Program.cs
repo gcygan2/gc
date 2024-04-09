1824dd8ef7adfe4624bd7b023c0aa9cd8f10050f
gcygan2 <114261165+gcygan2@users.noreply.github.com>
2024-04-09 19:38:10 +0200

    09042024

---
+++ Switch/Switch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj nazwę postaci");
+            string nazwa = Console.ReadLine();
+            Thread.Sleep(3000);
+            switch (nazwa.ToLower())
+            {
+                case "gryf":
+                    Console.WriteLine("Gryf z wyglądu jest połączeniem lwa oraz orła.");
+                    break;
+                default:
+                    Console.WriteLine("Nie znam tej postaci.");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
